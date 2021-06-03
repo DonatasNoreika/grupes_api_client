a8ab2bf60ce65afde75e844c344ab77919391535
Donoras <donoras@gmail.com>
2021-06-03 06:04:04 +0300

    Added registration

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 # iš flask bibliotekos importuojame klasę Flask ir f-ją render_template.
 app = Flask(__name__)
 import requests
 import json
-from forms import BandForm
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+# nustatėme, kad mūsų duomenų bazė bus šalia šio failo esants data.sqlite failas
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# neseksime kiekvienos modifikacijos
+db = SQLAlchemy(app)
+# pilnas kelias iki šio failo.
+
+if __name__ == '__main__':
+    from forms import BandForm, RegistracijosForma, PrisijungimoForma
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'registruotis'
+login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(vartotojo_id):
+    return Vartotojas.query.get(int(vartotojo_id))
 
 app.config['SECRET_KEY'] = 'bet kokia simbolių eilutė'
 # inicijuojame klasės Flask objektą, priskiriame kintamąjam app.
 
+class Vartotojas(db.Model, UserMixin):
+    __tablename__ = "vartotojas"
+    id = db.Column(db.Integer, primary_key=True)
+    vardas = db.Column("Vardas", db.String(20), unique=True, nullable=False)
+    el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
+    slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
+    token = db.Column("Žetonas", db.String, nullable=True)
+
 @app.route("/bands")
+@app.route("/")
 def bands():
     r = requests.get("http://127.0.0.1:8000/bands")
     bands = json.loads(r.text)
     return render_template('bands.html', bands=bands)
 
+@login_required
 @app.route('/add_band', methods=['GET', 'POST'])
 def add_band():
     form = BandForm()
@@ -26,5 +61,42 @@ def add_band():
         return redirect(url_for('bands'))
     return render_template('add_band.html', form=form)
 
+@app.route("/registruotis", methods=['GET', 'POST'])
+def registruotis():
+    db.create_all()
+    if current_user.is_authenticated:
+        return redirect(url_for('bands'))
+    form = RegistracijosForma()
+    if form.validate_on_submit():
+        koduotas_slaptazodis = bcrypt.generate_password_hash(form.slaptazodis.data).decode('utf-8')
+        vartotojas = Vartotojas(vardas=form.vardas.data, el_pastas=form.el_pastas.data, slaptazodis=koduotas_slaptazodis)
+        db.session.add(vartotojas)
+        db.session.commit()
+        flash('Sėkmingai prisiregistravote! Galite prisijungti', 'success')
+        return redirect(url_for('bands'))
+    return render_template('registruotis.html', title='Register', form=form)
+
+
+@app.route("/prisijungti", methods=['GET', 'POST'])
+def prisijungti():
+    if current_user.is_authenticated:
+        return redirect(url_for('bands'))
+    form = PrisijungimoForma()
+    if form.validate_on_submit():
+        user = Vartotojas.query.filter_by(el_pastas=form.el_pastas.data).first()
+        if user and bcrypt.check_password_hash(user.slaptazodis, form.slaptazodis.data):
+            login_user(user, remember=form.prisiminti.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('bands'))
+        else:
+            flash('Prisijungti nepavyko. Patikrinkite el. paštą ir slaptažodį', 'danger')
+    return render_template('prisijungti.html', title='Prisijungti', form=form)
+
+@app.route("/atsijungti")
+def atsijungti():
+    logout_user()
+    return redirect(url_for('bands'))
+
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=5000, debug=True)
+    db.create_all()
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import SubmitField, BooleanField, StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, ValidationError, EqualTo
 
+from app import app
 
 class BandForm(FlaskForm):
     token = StringField('Token', [DataRequired()])
     name = StringField('Name', [DataRequired()])
     submit = SubmitField('Submit')
+
+
+class RegistracijosForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", [EqualTo('slaptazodis', "Slaptažodis turi sutapti.")])
+    submit = SubmitField('Prisiregistruoti')
+
+    def tikrinti_varda(self, vardas):
+        vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+        if vartotojas:
+            raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+
+    def tikrinti_pasta(self, el_pastas):
+        vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+        if vartotojas:
+            raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+
+
+class PrisijungimoForma(FlaskForm):
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    prisiminti = BooleanField("Prisiminti mane")
+    submit = SubmitField('Prisijungti')
